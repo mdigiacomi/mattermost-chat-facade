759db63e26414e755c14af8f4bc2b0b83e1ea8da
Michael Di Giacomi <digitaladrenalin@gmail.com>
2015-12-07 18:46:18 -0500

    Initial Commit

---
+++ routes/mattermost.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+var returnVal = {posts: [{}]};
+var token = "";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  
+  request({ method: 'POST',
+            uri: 'https://chat.digitaladrenalin.net/api/v1/users/login',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({name:"dadrenalin",email:"mdigiac1@me.com",password:"t3AjxB5p"})
+          },
+    function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      
+      token = response.headers.token;
+      
+      request({ method: 'GET',
+                uri: 'https://chat.digitaladrenalin.net/api/v1/channels/gpxucn1bp3ra3ci7k5szte3eow/posts/0/100',
+                headers: {Authorization: "Bearer " + token }
+      }, function(error, response, body){
+        
+        var parsebody = JSON.parse(body);
+
+        var posts = parsebody.posts;
+        var order = parsebody.order;
+        
+        request({ method: 'GET',
+                uri: 'https://chat.digitaladrenalin.net/api/v1/channels/gpxucn1bp3ra3ci7k5szte3eow/extra_info',
+                headers: {Authorization: "Bearer " + token }
+        }, function(error, response, body){
+          
+          var members = JSON.parse(body);
+          
+          console.log(order.length);
+          console.log(members);
+
+          for(var i = 0; i < order.length; i++)
+          {
+            var post = {};
+            
+            post.message = posts[order[i]].message
+            
+            if(posts[order[i]].props.length > 1)
+            {
+              post.user = posts[order[i]].props.override_username;
+            }
+            else
+            {
+              for(var x = 0; x < members.members.length; x++)
+              {
+                if(posts[order[i]].user_id === members.members[x].id)
+                {
+                  post.user = members.members[x].username;
+                }
+              }
+            }
+            returnVal.posts.push(post);
+          }
+          res.setHeader('Content-Type', 'application/json');
+          res.status(200).send(returnVal);
+        });
+      });
+    }
+    else
+    {
+      console.log(error);
+      console.log(body);
+      console.log(next);
+      res.sendStatus(404);
+    }
+  });
+});
+
+module.exports = router;
